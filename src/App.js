d93963f8a813f0c1679e16928631df668307dda8
hamuuri <mohammed_125@outlook.com>
2022-09-27 16:33:08 +0700

    updated some files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import './App.css';
 
 
 class App extends React.Component {
 	render() {
 		return(
 	<center>
-		<div class="logo">
-			<img alt="Google" src="img/google.png" />
+		<div className="logo">
+			<img alt="Google" src="/google.png" />
 		</div>
 
 
 	<form action="/results" method="post">
-		<input class="searchbar" type="search" name="search" />
-		<div class="buttons">
+		<input className="searchbar" type="search" name="search" />
+		<div className="buttons">
 			<button type="submit">Search</button>
 		</div>
 	</form>
