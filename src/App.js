130b5f52a39d860c3590836381547a3904c3ab08
hamuuri <mohammed_125@outlook.com>
2022-09-27 16:12:27 +0700

    added html to app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import React from 'react'
 class App extends React.Component {
 	render() {
 		return(
+	<center>
+		<div class="logo">
+			<img alt="Google" src="img/google.png" />
+		</div>
+
+
+	<form action="/results" method="post">
+		<input class="searchbar" type="search" name="search" />
+		<div class="buttons">
+			<button type="submit">Search</button>
+		</div>
+	</form>
+</center>
 
 		)
 	}
