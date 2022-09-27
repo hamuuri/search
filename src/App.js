17baa06e40e7980e331645de88e0cdb067a5e713
vmmaturana <vmmaturana@gmail.com>
2022-09-27 17:10:46 +0700

    Updated APP to Record in State the Input of SEARCH

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import './App.css';
-
+import React from "react";
+import "./index.css";
 
 class App extends React.Component {
-	render() {
-		return(
-	<center>
-		<div className="logo">
-			<img alt="Google" src="/google.png" />
-		</div>
+  state = {
+    search: "",
+  };
+  changeSearch = (e) => {
+    {
+      this.setState({ search: e.target.value });
+    }
+  };
+  render() {
+    return (
+      <center>
+        <div className="logo">
+          <img alt="Google" src="/google.png" />
+        </div>
 
-
-	<form action="/results" method="post">
-		<input className="searchbar" type="search" name="search" />
-		<div className="buttons">
-			<button type="submit">Search</button>
-		</div>
-	</form>
-</center>
-
-		)
-	}
+        <form action="/results" method="post">
+          <input
+            className="searchbar"
+            type="search"
+            name="search"
+            onKeyUp={this.changeSearch}
+          />
+          <div className="buttons">
+            <button type="submit">Search</button>
+          </div>
+        </form>
+      </center>
+    );
+  }
 }
 
 export default App;
